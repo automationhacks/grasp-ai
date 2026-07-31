44c85e5794772d32fce5ff89ed32e9fa733fb687
Gaurav Singh <gauravsing@microsoft.com>
2026-07-31 11:58:21 +0530

    Updated the prompt to list prominent members in python ecosystem.

--- src/main/agent-framework/samples/agent-framework-101/01-basic-chat-example.py
+++ src/main/agent-framework/samples/agent-framework-101/01-basic-chat-example.py
@@ -15,6 +15,7 @@ project = AIProjectClient(
 openai = project.get_openai_client()
 
 responses = openai.responses.create(
-    model="gpt-5.4-mini", input="Who created python and list the top 5 PSF members?"
+    model="gpt-5.4-mini",
+    input="Who created python and list the the most influential people in the Python community?",
 )
 print(f"Response output: {responses.output_text}")
